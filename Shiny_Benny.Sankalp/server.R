8fbd3b91aadd5d0b9d3401511ede8f9d01cef57d
Sankalp Sinha <sansinha@ucdavis.edu>
2019-05-09 22:41:56 +0000

    adding column names changes to the server file

--- Shiny_Benny.Sankalp/server.R
+++ Shiny_Benny.Sankalp/server.R
@@ -12,8 +12,21 @@ library(tidyverse)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  data.pheno <- read_csv("/home/ubuntu/RiceSNPData/RiceDiversity.44K.MSU6.Phenotypes.csv", na=c("NA", "00")) 
+  data.pheno <- read_csv("/home/ubuntu/Assignments/Shiny_Benny.Sankalp/Shiny_Benny.Sankalp/RiceDiversity.44K.MSU6.Phenotypes.csv", na=c("NA", "00")) 
   #head(data.pheno)
+  
+  #changing the column names to pull information for graphing
+  colnames(data.pheno)[colnames(data.pheno)=="Protein content"] <- "protein.content"
+  colnames(data.pheno)[colnames(data.pheno)=="Amylose content"] <- "amylose.content"
+  colnames(data.pheno)[colnames(data.pheno)=="Alu.Tol"] <- "alu.tol"
+  colnames(data.pheno)[colnames(data.pheno)=="Plant height"] <- "plant.height"
+  colnames(data.pheno)[colnames(data.pheno)=="Seed surface area"] <- "seed.surface.area"
+  colnames(data.pheno)[colnames(data.pheno)=="Seed volume"] <- "seed.volume"
+  colnames(data.pheno)[colnames(data.pheno)=="Panicle number per plant"] <- "panicle.number.per.plant"
+  colnames(data.pheno)[colnames(data.pheno)=="Seed length"] <- "seed.length"
+  colnames(data.pheno)[colnames(data.pheno)=="Brown rice volume"] <- "brown.rice.volume"
+  colnames(data.pheno)[colnames(data.pheno)=="Panicle length"] <- "panicle.length"
+  
   
   output$scatterplot <- renderPlot({
     
@@ -24,7 +37,7 @@ shinyServer(function(input, output) {
     # draw the histogram with the specified number of bins
     #hist(x, breaks = bins, col = 'blue', border = 'black')
     
-    # set up the plot
+    #set up the scatter plot
     plot <- ggplot(data = data.pheno, aes_string(x=input$trait.1, y=input$trait.2, color=input$trait.3))
     plot + geom_point()
     
